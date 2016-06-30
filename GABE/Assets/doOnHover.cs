87e603a3ac09cbc683dc63bfd316e467d8537f36
sarahrothberg <sarahrothberg@gmail.com>
2016-06-29 20:22:58 -0400

    got audio stuff working , still needs fade

---
+++ GABE/Assets/doOnHover.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using VRStandardAssets.Utils;
+
+namespace VRStandardAssets.Examples
+{
+	
+public class doOnHover : MonoBehaviour {
+		        
+			[SerializeField] private VRInteractiveItem m_InteractiveItem;
+
+			private void Awake ()
+			{
+			AudioSource audio = GetComponent<AudioSource>();
+
+			}
+
+			private void OnEnable()
+			{
+				m_InteractiveItem.OnOver += HandleOver;
+				m_InteractiveItem.OnOut += HandleOut;
+				m_InteractiveItem.OnClick += HandleClick;
+				m_InteractiveItem.OnDoubleClick += HandleDoubleClick;
+			}
+
+
+			private void OnDisable()
+			{
+				m_InteractiveItem.OnOver -= HandleOver;
+				m_InteractiveItem.OnOut -= HandleOut;
+				m_InteractiveItem.OnClick -= HandleClick;
+				m_InteractiveItem.OnDoubleClick -= HandleDoubleClick;
+			}
+
+
+			//Handle the Over event
+			private void HandleOver()
+			{
+				Debug.Log("Show over state");
+			GetComponent<AudioSource>().volume=1f;
+
+			}
+
+
+			//Handle the Out event
+			private void HandleOut()
+			{
+				Debug.Log("Show out state");
+			GetComponent<AudioSource>().volume=0.01f;
+
+
+			}
+
+
+			//Handle the Click event
+			private void HandleClick()
+			{
+				Debug.Log("Show click state");
+			}
+
+
+			//Handle the DoubleClick event
+			private void HandleDoubleClick()
+			{
+				Debug.Log("Show double click");
+			}
+		}
+
+	}
