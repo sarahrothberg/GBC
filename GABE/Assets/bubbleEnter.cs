8ee69d2c42642bb10a1fb6b743a63b392624d1a0
sarah rothberg <sarah rothberg>
2016-07-24 22:43:16 -0400

    most recent
    most recent

---
+++ GABE/Assets/bubbleEnter.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class bubbleEnter : MonoBehaviour {
+
+    public Transform player;
+    public Transform bubblePosition;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+    void OnTriggerEnter(Collider bubble)
+    {
+        if (bubble.gameObject.tag == "Bubble")
+         {
+            Debug.Log("enter bubble");
+            player.localScale = new Vector3(0.02F, 0.02F, 0.02F);
+            player.position = bubblePosition.position;
+        }
+
+    }
+}
