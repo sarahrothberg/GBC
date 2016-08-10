69b293f74724aa1f67154d1744d8fbcf4684ec62
ROTHBERG <sarahrothberg@gmail.com>
2016-08-10 01:19:14 -0400

    added elevator stuff

---
+++ GABE/Assets/enterDoor.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class enterDoor : MonoBehaviour {
+
+    Animator anim;
+    public AudioClip sound;
+
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+	
+	}
+
+    void OnTriggerEnter()
+    {
+        anim.SetTrigger("DoorOpen");
+    }
+
+    void OnTriggerExit()
+    {
+        anim.SetTrigger("DoorClose");
+    }
+}
+
---
+++ GABE/Assets/personSelect.cs
@@ -0,0 +1,88 @@
+﻿using UnityEngine;
+using System.Collections;
+using VRStandardAssets.Utils;
+
+namespace VRStandardAssets.Examples
+{
+
+    public class personSelect : MonoBehaviour
+    {
+        
+        float startTime;
+        float endTime;
+        float fadeTime = 1f;
+        bool fadeIn = false;
+        float volume;
+        public GameObject player;
+        public GameObject person;
+
+        public GameObject[] otherPeople;
+        public GameObject[] otherButtons;
+
+
+
+        [SerializeField]
+        private Material m_NormalMaterial;
+        [SerializeField]
+        private Material m_OverMaterial;
+        [SerializeField]
+        private VRInteractiveItem m_InteractiveItem;
+        [SerializeField]
+        private Renderer m_Renderer;
+
+        private Vector3 waypointPos;
+
+        private void Awake()
+        {
+            m_Renderer.material = m_NormalMaterial;
+        }
+
+        private void OnEnable()
+        {
+            m_InteractiveItem.OnOver += HandleOver;
+            m_InteractiveItem.OnOut += HandleOut;
+
+        }
+
+
+        private void OnDisable()
+        {
+            m_InteractiveItem.OnOver -= HandleOver;
+            m_InteractiveItem.OnOut -= HandleOut;
+        }
+
+        void Update()
+        {
+           
+        }
+
+
+        //Handle the Over event
+        private void HandleOver()
+        {
+            for (int i = 0; i < otherPeople.Length; i++)
+            {
+                otherPeople[i].SetActive(false);
+                Renderer temp = otherButtons[i].GetComponent<Renderer>();
+                temp.material = m_NormalMaterial;
+            }
+            Debug.Log("waypoint name is " + person);
+            m_Renderer.material = m_OverMaterial;
+            person.SetActive(true);
+
+            
+        }
+
+
+        //Handle the Out event
+        private void HandleOut()
+        {
+            Debug.Log("out");
+            //fadeIn = false;
+            //startTime = Time.time;
+            //endTime = Time.time + fadeTime;
+
+
+        }
+    }
+}
